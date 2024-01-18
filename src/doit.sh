0864d67d271adf3d66f29c9e5ed7fe6d6468cdc2
FranPuentes <fran@puentescalvo.com>
2024-01-18 11:16:21 +0100

    Cambio de nombre

--- src/doit.sh
+++ src/doit.sh
@@ -3,13 +3,16 @@
 # TODO hacer logs
 # TODO mandar notificaciones
 
+DATA="../data"
 TEMP="../tmp"
 
-(./create_datasets.py   && \
- ./preprocess.py        && \
- ./variations.py        && \
- ./dump_datasets.py     && \
- ./train_datasets.py    && \
-./evaluate_datasets.py) > ${TEMP}/out.log 2> ${TEMP}/err.log
+mkdir -p "${TEMP}"
+
+(./create.py      "${DATA}" "${TEMP}" && \
+ ./preprocess.py  "${DATA}" "${TEMP}" && \
+ ./variations.py  "${DATA}" "${TEMP}" && \
+ ./dump.py        "${DATA}" "${TEMP}" && \
+ ./train.py       "${DATA}" "${TEMP}" && \
+ ./evaluate.py    "${DATA}" "${TEMP}"    ) > "${TEMP}/out.log" 2> "${TEMP}/err.log"
 
 
